a8be607a455476320c653245e5e2d41cbdfde692
Jyot <21ce023@charusat.edu.in>
2024-05-22 12:55:45 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Form from './Form';
+import Details from './Details';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
